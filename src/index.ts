9916830ba4d365909ac80a51d88074b234fd1c52
val.istar.guo <val.istar.guo@gmail.com>
2022-04-11 11:37:08 +0800

    fix: cannot work with keq-cli

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import { Middleware } from 'keq'
 
 
-/**
- * Modules exported from this file can be used by users
- */
+function format(pathname: string): string {
+  return decodeURIComponent(pathname).replace(/{(.*?)}/g, ':$1')
+}
 
-export default function SwaggerPath(): Middleware {
+export default function urlTemplate(): Middleware {
   return async(ctx, next) => {
-    ctx.url.pathname = ctx.url.pathname.replace(/{(.*?)}/g, ':$1'),
+    ctx.url.pathname = format(ctx.url.pathname)
+
+    if (ctx.options.module && ctx.options.module.pathname) {
+      ctx.options.module.pathname = format(ctx.options.module.pathname)
+    }
 
     await next()
   }
